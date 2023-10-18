69cc157bd49de4a1dde5bbedca5decaed68d6dfe
hykim <hiyoung.kimmy@gmail.com>
2023-10-17 22:29:12 -0700

    Fixed Onboarding Page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import AccountProfile from "@/components/form/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { fetchUser } from '@/lib/actions/user.action';
+import { redirect } from 'next/navigation';
 
 async function Page() {
   const user = await currentUser();
-
-  const userInfo = {};
+  if (!user) return null;
+  
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect('/');
 
   const userData = {
     id: user.id,
